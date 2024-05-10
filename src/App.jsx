fe47db48db77437289c65a0480837d4f7b77e7be
mohalw731 <mohalw731@gmail.com>
2024-05-10 08:18:33 +0200

    lazy error

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
-import { lazy } from 'react'
 import Home from './pages/Home'
-const Contact = lazy(() => import('./pages/Contact'))
+import Jobs from './pages/Jobs'
+import Contact from './pages/Contact'
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-const Jobs = lazy(() => import('./pages/Jobs'))
+
 
 
 function App() {
